d5fd6bd089ced76e3f100f3fb5471537f85e54ba
Alexander Brandt <alexander.brandt@outerstellar.de>
2026-04-11 18:33:10 +0300

    feat: add DocCategory and DocTarget model types

---
+++ needlecast-desktop/src/main/kotlin/io/github/rygel/needlecast/model/DocCategory.kt
@@ -0,0 +1,8 @@
+package io.github.rygel.needlecast.model
+
+enum class DocCategory(val displayName: String) {
+    API_DOCS("API Docs"),
+    TEST_REPORTS("Test Reports"),
+    COVERAGE("Coverage"),
+    SITE("Site"),
+}
